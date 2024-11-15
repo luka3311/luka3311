ca761808a83ba4b0ded7255a305f19931d8ee6e9
Parać Luka <0135279073@FPZ.HR>
2024-11-15 15:13:09 +0100

    druga vjezba

--- vj5.py
+++ vj5.py
@@ -10,3 +10,19 @@ if __name__ == "__main__":
         print(provjera_broja(uneseni broj))
     except ValueError:
         print("unesena vrijednost nije broj.")
+
+def provjera_broja(broj):
+    if 10 <= broj <= 100:
+        return f"broj {broj} je unutar raspona."
+    else:
+        return f"broj {broj} je izvan raspona."
+    
+if __name__ == "__main__":
+    try:
+        prvi_broj = int(input("unesite prvi broj:"))
+        print(provjera_broja(prvi_broj))
+        drugi_broj = int(input("unesite drugi broj:"))
+        print(provjera_broja(drugi_broj))
+    except ValueError:
+        print("unesena vrijednost nije broj.")
+
